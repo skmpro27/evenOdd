4e30bc4f069182834a5fe6a84696c7972625506d
unknown <kumar.skm03@gmail.com>
2021-02-15 10:34:02 +0530

    added evenOdd.sh

--- evenOdd.sh
+++ evenOdd.sh
@@ -5,3 +5,13 @@ read -p "Enter 1st number " num1
 read -p "Enter 2nd number " num2
 read -p "Enter 3rd number " num3
 
+for number in $num1 $num2 $num3
+do
+        mod=$(( number%2 ))
+        if [ $mod -eq 0 ]
+        then
+                echo $number "is even"
+        else
+                echo $number "is odd"
+        fi
+done
